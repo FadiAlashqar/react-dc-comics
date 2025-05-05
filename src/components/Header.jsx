25672f8a0e1e948e4c84caaeecf99958b4811318
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-06 00:38:00 +0200

    [UPD] header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <figure>
+                <img src="favicon.ico" alt="" />
+            </figure>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="#" className='active'>COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="#">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="#">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="#">FANS</a>
+                    </li>
+                    <li>
+                        <a href="#">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="#">SHOP</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
