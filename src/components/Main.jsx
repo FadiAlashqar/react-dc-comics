6d5061485b80f23d9cab15432cf14ac5c9c60c25
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-06 01:54:36 +0200

    [UPD] main 1.2 done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,38 @@
 const Main = () => {
     return (
         <main>
-            <p>&#45;&#45;&gt; Content goes here &lt;&#45;&#45;</p>
+            <div className="content">
+                <p>&#45;&#45;&gt; Content goes here &lt;&#45;&#45;</p>
+            </div>
+            <div className="icons">
+                <ul>
+                    <li>
+                        <img className="icon" src="../src/assets/img/buy-comics-digital-comics.png" alt="" />
+                        DIGITAL COMICS
+
+                    </li>
+                    <li>
+                        <img className="icon" src="../src/assets/img/buy-comics-merchandise.png" alt="" />
+                        DC MERCHANDISE
+
+                    </li>
+                    <li>
+                        <img className="icon" src="../src/assets/img/buy-comics-subscriptions.png" alt="" />
+                        SUBSCRIPTION
+
+                    </li>
+                    <li>
+                        <img className="icon" src="../src/assets/img/buy-comics-shop-locator.png" alt="" />
+                        COMIC SHOP LOCATOR
+
+                    </li>
+                    <li>
+                        <img className="icon" src="../src/assets/img/buy-dc-power-visa.svg" alt="" />
+                        DC POWER VISA
+
+                    </li>
+                </ul>
+            </div>
         </main>
     )
 }
