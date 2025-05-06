aaaf731fb632a556947b5bfa5a33a3d777aa7a68
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-06 02:32:29 +0200

    [UPD] footer 1.1 done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+
+
+function Footer() {
+    return (
+        <footer>
+            <div className="links">
+                <div className="footer-col">
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div className="footer-col">
+                    <h4>DC</h4>
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy &#40;New	&#41;</li>
+                        <li>Ad choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subcrictions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div className="footer-col">
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
