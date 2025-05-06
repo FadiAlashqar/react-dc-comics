15cc6f91b6d4a4753151342d992f96a15245fa78
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-06 16:31:25 +0200

    [UPD] jumbotron done

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Jumbotron = () => {
+    return (
+        <figure className='jumbo'>
+            <img src="../src/assets/img/jumbotron.jpg" alt="" />
+        </figure>
+    )
+}
+
+export default Jumbotron
